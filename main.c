976e1ac49769a13436487c562c0e1e393c89d659
nem0301 <sks156324@gmail.com>
2017-02-01 14:01:57 +0900

    add EEPROM read/write

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <avr/io.h>
+#include <avr/eeprom.h>
 #include <util/delay.h>
 
 #define F_CPU 16000000UL
 #define BAUD 57600
-
 #include <util/setbaud.h>
 
 #include <stdio.h>
@@ -28,16 +28,17 @@ static char lbracket = 0;
 char prompt[20] = "console > ";
 
 typedef struct {
-    char text[80];
+    char text[30];
 }Console;
 
 Console console[20];
 // cursors position
 int x, y;
+int cy = -1;
 
 void uart_init(void);
-void uart_putchar(char c, FILE *stream);
-char uart_getchar(FILE *stream);
+int uart_putchar(char c, FILE *stream);
+int uart_getchar(FILE *stream);
 void clearScreen();
 void setCursor(int x, int y);
 void specialKey(char c);
@@ -89,7 +90,7 @@ void uart_init(void)
 	UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */
 }
 
-void uart_putchar(char c, FILE *stream)
+int uart_putchar(char c, FILE *stream)
 {
     if (c == '\n')
 	{
@@ -99,12 +100,13 @@ void uart_putchar(char c, FILE *stream)
 	loop_until_bit_is_set(UCSR0A, UDRE0);
 
     UDR0 = c;
+    return c;
 }
 
-char uart_getchar(FILE *stream)
+int uart_getchar(FILE *stream)
 {
 	loop_until_bit_is_set(UCSR0A, RXC0);
-	return UDR0;
+	return (char)UDR0;
 }
 
 void clearScreen()
@@ -121,8 +123,6 @@ void setCursor(int x, int y)
 
 void key(char c)
 {
-
-
 	switch(c)
 	{
 	case 13:
@@ -137,6 +137,7 @@ void key(char c)
         }
 
 	    printf("%s", prompt);
+	    cy = -1;
 		break;
 	case 8:
 		if (x > 0)
@@ -144,6 +145,7 @@ void key(char c)
             putchar('\b');
             putchar(' ');
             x--;
+            console[y].text[x] = '\0';
             putchar(c);
         }
 		break;
@@ -175,6 +177,41 @@ void key(char c)
                 putchar('D');
             }
 
+            if (c == 65)
+            {
+                if (cy == -1) cy = y;
+                printf("%c[2K\r", 27);
+                printf("c%d %d\n", cy, y);
+                cy = cy - 1;
+                if (cy < 0)
+                    cy = 20 - cy;
+
+                if (strlen(console[cy].text) <= 0)
+                {
+                    printf("a%d %d\n", cy, y);
+                    cy = (cy + 1) % 20;
+                }
+                printf("%s%s", prompt, console[cy].text);
+                strcpy(console[y].text, console[cy].text);
+                x = strlen(console[y].text);
+            }
+            else if ( c == 66)
+            {
+                if (cy == -1) cy = y;
+                printf("%c[2K\r", 27);
+                cy = (cy + 1) % 20;
+                if (strlen(console[cy].text) <= 0)
+                {
+                    cy = cy - 1;
+                    if (cy < 0)
+                        cy = 20 - cy;
+                }
+
+                printf("%s%s", prompt, console[cy].text);
+                strcpy(console[y].text, console[cy].text);
+                x = strlen(console[y].text);
+            }
+
             esc = 0;
             lbracket = 0;
             return;
@@ -185,29 +222,54 @@ void key(char c)
             lbracket = 0;
         }
 
-	    console[y].text[x] = c;
-		x++;
+        if (x < 29)
+        {
+            console[y].text[x] = c;
+            x++;
+        }
 		putchar(c);
 		console[y].text[x] = '\0';
 		break;
 	}
 }
 
-#define CMD_NUM     4
+#define CMD_NUM     6
 char *cmdList[CMD_NUM] =
 {
     "history",
     "clear",
     "ls",
-    "set"
+    "spi",
+    "rwb",
+    "rww"
 };
 
-void func(char *cmd)
+void func(char *args)
 {
+    char *token;
+    char *rest;
+    char str[30];
+    char *argv[10];
+    int argc = 0;
+    int i, j;
+
+    memset(str, '\0', sizeof(str));
+    strcpy(str, args);
+    rest = str;
+
+    while((token = strtok_r(rest, " ", &rest)))
+    {
+        int len = strlen(token);
+        argv[argc] = (char*) malloc (len + 1);
+        strcpy(argv[argc], token);
+        argv[argc][len] = '\0';
+        argc++;
+    }
+
+    char *cmd = argv[0];
+
     if (strcmp(cmd, "history") == 0)
     {
-        int i = 0;
-        int j = 0;
         for (j = 0, i = y + 1; j <20 ; i++, j++)
         {
             printf("%2d \t %s\n", j + 1, console[i%20].text);
@@ -219,23 +281,93 @@ void func(char *cmd)
     }
     else if (strcmp(cmd, "ls") == 0)
     {
-        int i= 0;
         for (i = 0; i < CMD_NUM; i++)
         {
             printf("%10s", cmdList[i]);
         }
         putchar('\n');
     }
-    #ifdef MASTER
-    else if (strcmp(cmd, "set 1") == 0)
+    else if (strcmp(cmd, "rwb") == 0 || strcmp(cmd, "rww") == 0)
     {
-        printf("set 1\n");
-        printf("ret %d\n", masterWrite(1));
+        if (argc < 3)
+        {
+            printf("usage rw(b|w) (-r|-w) addr [value]\n");
+        }
+        else
+        {
+            int addr = (int)strtol(argv[2], NULL, 0);
+            uint8_t byte;
+            uint16_t word;
+
+            if (strcmp(argv[1], "-r") == 0)
+            {
+                if (strcmp(cmd, "rwb") == 0)
+                {
+                    byte = eeprom_read_byte((uint8_t*)addr);
+                    printf("addr : 0x%04x,  value : 0x%02x\n", addr, byte);
+                }
+                else if (strcmp(cmd, "rww") == 0)
+                {
+                    word = eeprom_read_word((uint16_t*)addr);
+                    printf("addr : 0x%04x,  value : 0x%04x\n", addr, word);
+                }
+
+            }
+            else if (strcmp(argv[1], "-w") == 0)
+            {
+                if (argc < 4)
+                {
+                    printf("usage rw(b|w) (-r|-w) hexAddr [hexValue]\n");
+                }
+                else
+                {
+                    if (strcmp(cmd, "rwb") == 0)
+                    {
+                        byte = (uint8_t)strtol(argv[3], NULL, 0);
+                        eeprom_write_byte((uint8_t*)addr, (uint8_t)byte);
+                        byte = eeprom_read_byte((uint8_t*)addr);
+                        printf("addr : 0x%04x,  value : 0x%02x\n", addr, byte);
+                    }
+                    else if (strcmp(cmd, "rww") == 0)
+                    {
+                        word = (uint16_t)strtol(argv[3], NULL, 0);
+                        eeprom_write_word((uint16_t*)addr, (uint16_t)word);
+                        word = eeprom_read_word((uint16_t*)addr);
+                        printf("addr : 0x%04x,  value : 0x%04x\n", addr, word);
+                    }
+                }
+            }
+
+
+        }
     }
-    else if (strcmp(cmd, "set 0") == 0)
+    #ifdef MASTER
+    else if (strcmp(cmd, "spi") == 0)
     {
-        printf("set 0\n");
-        printf("ret %d\n", masterWrite(0));
+        if (argc < 2)
+        {
+            printf("usage spi number\n");
+            for (i = 0; i < argc; i++)
+            {
+                free(argv[i]);
+            }
+            return;
+        }
+
+        for (i = 0; i < strlen(argv[1]); i++)
+        {
+            if (argv[1][i] < '0' || argv[1][i] > '9')
+            {
+                printf("arg format error : %s \n", argv[1]);
+                for (i = 0; i < argc; i++)
+                {
+                    free(argv[i]);
+                }
+                return;
+            }
+        }
+
+        printf("ret %d\n", masterWrite(atoi(argv[1])));
     }
     #endif // MASTER
     else
@@ -243,7 +375,12 @@ void func(char *cmd)
         printf("undefine comman \"%s\"\n", cmd);
     }
 
+    for (i = 0; i < argc; i++)
+    {
+        free(argv[i]);
+    }
 }
+
 
 int main(void)
 {
@@ -258,7 +395,6 @@ int main(void)
 #ifndef MASTER
     unsigned char n = 0;
 #endif // MASTER
-
     for(;;){
         /* insert your main loop code here */
 
