ee27b54d3ba1db82aa0d1997d56e78f869343d22
Gyusik Shin <sks156324@gmail.com>
2017-07-23 05:13:20 -0700

    add text on panel

--- oled/hdr/oled.h
+++ oled/hdr/oled.h
@@ -1,8 +1,11 @@
 #ifndef __OLED_h__
 #define __OLED_h__
+#include <string.h>
+#include <stdio.h>
 
 #include <avr/io.h>
 #include <util/delay.h>
+
 
 #define OLED_DDR 	DDRB
 #define OLED_PORT	PORTB
@@ -25,7 +28,10 @@
 #define OLED_CYAN	0, 127, 127
 
 
-void oledCommand(unsigned char cmd)
+void scrollV(uint8_t n);
+void genChar();
+
+void oledCommand(uint8_t cmd)
 {
 	OLED_PORT &= ~_BV(OLED_DC);
 
@@ -33,7 +39,7 @@ void oledCommand(unsigned char cmd)
 	while (!(SPSR & _BV(SPIF)));
 }
 
-void oledData(unsigned char data)
+void oledData(uint8_t data)
 {
 	OLED_PORT |= _BV(OLED_DC);
 
@@ -61,35 +67,73 @@ void oledInit()
 	_delay_ms(300);
 
 	// setting rgb
-	uint8_t data = 0b10100000;
+	uint8_t data = 0b10110000;
 	
 	oledCommand(0xa0);
 	oledData(data);
+
+	scrollV(32);
+
+	// enhanced display
+	oledCommand(0xb2);
+	oledData(0xa4);
+	oledData(0x00);
+	oledData(0x00);
+
+	drawRect(0, 0, 128, 128, OLED_WHITE);
+	_delay_ms(500);
+	drawRect(0, 0, 128, 128, OLED_BLACK);
+	_delay_ms(500);
 }
 
 
-void scrollV(unsigned char n)
+void scrollV(uint8_t n)
 {
 	oledCommand(0xa1);
 	oledData(n);
 }
 
+// dir -1 : right, 0 : stop, 1 : left
+// start : start row
+// num : number of rows
+// spped 0 : fast, 1 : normal, 2 : slow, 3 : slowest
+void scrollH(uint8_t dir, uint8_t start, uint8_t num, uint8_t speed)
+{
+	oledCommand(0x96);
+	oledData(dir);
+	oledData(start);
+	oledData(num);
+	oledData(0);
+	oledData(speed);
+}
+
+void startScrollH()
+{
+	oledCommand(0x9f);
+}
+
+void stopScrollH()
+{
+	oledCommand(0x9e);
+}
+
+
 // 0 : all off, 1 : all on, 2 : reset normal display, 3 : inverse
-void displayMode(unsigned char mode)
+void displayMode(uint8_t mode)
 {
 	if ( mode > 3 ) return;
 	oledCommand(0xa4 | mode);
 }
 
 // 0 : sleep, 1 : wake
-void sleepMode(unsigned char mode)
+void sleepMode(uint8_t mode)
 {
 	if ( mode > 1 ) return;
 	oledCommand(0xaE | mode);
 }
 
 // if value NULL then reset GS, else set value
-void grayScalePurseWidth(unsigned char *value)
+void grayScalePurseWidth(uint8_t *value)
 {
 	if ( value == NULL)
 	{
@@ -109,7 +153,15 @@ void grayScalePurseWidth(unsigned char *value)
 	}
 }
 
-void contrast(unsigned char value)
+void colorContrast(uint8_t r, uint8_t g, uint8_t b)
+{
+	oledCommand(0xc1);
+	oledData(b);
+	oledData(g);
+	oledData(r);
+}
+
+void contrast(uint8_t value)
 {
 	if ( value > 15 ) return;
 	oledCommand(0xc7);
@@ -133,9 +185,9 @@ void drawRect(uint8_t x, uint8_t y, uint8_t w, uint8_t h,
 	cursor(x, y, w, h);
 
 	oledCommand(0x5c);
-	for (int x = 0; x < w + 1; x++)
+	for (int x = 0; x < w; x++)
 	{
-		for (int y = 0; y < h + 1; y++)
+		for (int y = 0; y < h; y++)
 		{
 			oledData(b);
 			oledData(g);
@@ -144,51 +196,212 @@ void drawRect(uint8_t x, uint8_t y, uint8_t w, uint8_t h,
 	}
 }
 
+typedef struct {
+	uint8_t r;
+	uint8_t g;
+	uint8_t b;
+} color_t;
 
-
-void oledTest()
+void drawImage(uint8_t x, uint8_t y, uint8_t w, uint8_t h,
+		color_t *data)
 {
-//	for (int i = 0; i < 128; i++)
-//	{
-//		scrollV(i);
-//		_delay_ms(3);
-//	}
-//	_delay_ms(1000);
+	cursor(x, y, w, h);
 
-//
-//	for (int i = 0; i < 4; i++)
-//	{
-//		displayMode(i);
-//		_delay_ms(1000);
-//	}
-
-//	for (int i = 0; i < 2; i++)
-//	{
-//		sleepMode(i);
-//		_delay_ms(1000);
-//	}
+	oledCommand(0x5c);
+	int i = 0;
+	for (int x = 0; x < w; x++)
+	{
+		for (int y = 0; y < h; y++)
+		{
+			oledData(data[i].b);
+			oledData(data[i].g);
+			oledData(data[i].r);
+			i++;
+		}
+	}
+}
 
 
-//	unsigned char data[63];
-//	for (int i = 0; i < 63; i++)
-//	{
-//		data[i] = i * 2;
-//	}
-//	grayScalePurseWidth(data);
-//
-//	_delay_ms(1000);
-//
-//	grayScalePurseWidth(NULL);
-//
-//	_delay_ms(1000);
-//
+//uint8_t map[11 * 128];
+const uint8_t map[1408] =
+{
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
 
-	scrollV(32);
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
 
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,          //
+	12,30,30,30,12,12,0,12,12,0,0,  // !
+	54,54,20,0,0,0,0,0,0,0,0,       // "
+	0,18,18,63,18,18,63,18,18,0,0,  // #
+	12,30,51,3,30,48,51,30,12,12,0, // $
+	0,0,35,51,24,12,6,51,49,0,0,    // %
+	12,30,30,12,54,27,27,27,54,0,0, // &
+	12,12,6,0,0,0,0,0,0,0,0,        // '
+	24,12,6,6,6,6,6,12,24,0,0,      // (
+	6,12,24,24,24,24,24,12,6,0,0,   // )
+	0,0,0,51,30,63,30,51,0,0,0,     // *
+	0,0,0,12,12,63,12,12,0,0,0,     // +
+	0,0,0,0,0,0,0,12,12,6,0,        // ,
+	0,0,0,0,0,63,0,0,0,0,0,         // -
+	0,0,0,0,0,0,0,12,12,0,0,        // .    
+	0,0,32,48,24,12,6,3,1,0,0,      // /
+
+	12,30,51,51,51,51,51,30,12,0,0, // 0
+	12,14,15,12,12,12,12,12,63,0,0, // 1
+	30,51,48,24,12,6,3,51,63,0,0,   // 2
+	30,51,48,48,28,48,48,51,30,0,0, // 3
+	16,24,28,26,25,63,24,24,60,0,0, // 4
+	63,3,3,31,48,48,48,51,30,0,0,   // 5
+	28,6,3,3,31,51,51,51,30,0,0,    // 6
+	63,49,48,48,24,12,12,12,12,0,0, // 7
+	30,51,51,51,30,51,51,51,30,0,0, // 8
+	30,51,51,51,62,48,48,24,14,0,0, // 9
+
+	0,0,12,12,0,0,12,12,0,0,0,      // :
+	0,0,12,12,0,0,12,12,6,0,0,      // ;
+	0,0,24,12,6,3,6,12,24,0,0,      // <
+	0,0,0,63,0,0,63,0,0,0,0,        // =
+	0,0,3,6,12,24,12,6,3,0,0,       // >
+	30,51,51,59,59,59,27,3,30,0,0,  // @
+	30,51,51,24,12,12,0,12,12,0,0,  // ?
+
+	12,30,51,51,63,51,51,51,51,0,0,          // A ** TO BE FILLED **
+	31,51,51,51,31,51,51,51,31,0,0, // B
+	28,54,35,3,3,3,35,54,28,0,0,    // C
+	15,27,51,51,51,51,51,27,15,0,0, // D
+	63,51,35,11,15,11,35,51,63,0,0, // E
+	63,51,35,11,15,11,3,3,3,0,0,    // F
+	28,54,35,3,59,51,51,54,44,0,0,  // G
+	51,51,51,51,63,51,51,51,51,0,0, // H
+	30,12,12,12,12,12,12,12,30,0,0, // I
+	60,24,24,24,24,24,27,27,14,0,0, // J
+	51,51,51,27,15,27,51,51,51,0,0, // K
+	3,3,3,3,3,3,35,51,63,0,0,       // L
+	33,51,63,63,51,51,51,51,51,0,0, // M
+	51,51,55,55,63,59,59,51,51,0,0, // N
+	30,51,51,51,51,51,51,51,30,0,0, // O
+	31,51,51,51,31,3,3,3,3,0,0,     // P
+	30,51,51,51,51,51,63,59,30,48,0,// Q
+	31,51,51,51,31,27,51,51,51,0,0, // R
+	30,51,51,6,28,48,51,51,30,0,0,  // S
+	63,63,45,12,12,12,12,12,30,0,0, // T
+	51,51,51,51,51,51,51,51,30,0,0, // U
+	51,51,51,51,51,30,30,12,12,0,0, // V
+	51,51,51,51,51,63,63,63,18,0,0, // W
+	51,51,30,30,12,30,30,51,51,0,0, // X
+	51,51,51,51,30,12,12,12,30,0,0, // Y
+	63,51,49,24,12,6,35,51,63,0,0,  // Z
+
+	30,6,6,6,6,6,6,6,30,0,0,          // [
+	0,0,1,3,6,12,24,48,32,0,0,        // "\"
+	30,24,24,24,24,24,24,24,30,0,0,   // ]
+	8,28,54,0,0,0,0,0,0,0,0,          // ^
+	0,0,0,0,0,0,0,0,0,63,0,           // _
+	6,12,24,0,0,0,0,0,0,0,0,          // `
+
+	0,0,0,14,24,30,27,27,54,0,0,      // a
+	3,3,3,15,27,51,51,51,30,0,0,      // b
+	0,0,0,30,51,3,3,51,30,0,0,        // c
+	48,48,48,60,54,51,51,51,30,0,0,  // d
+	0,0,0,30,51,63,3,51,30,0,0,      // e
+	28,54,38,6,15,6,6,6,15,0,0,      // f
+	0,0,30,51,51,51,62,48,51,30,0,   // g
+	3,3,3,27,55,51,51,51,51,0,0,     // h
+	12,12,0,14,12,12,12,12,30,0,0,   // i
+	48,48,0,56,48,48,48,48,51,30,0,  // j
+	3,3,3,51,27,15,15,27,51,0,0,     // k
+	14,12,12,12,12,12,12,12,30,0,0,  // l
+	0,0,0,29,63,43,43,43,43,0,0,     // m
+	0,0,0,29,51,51,51,51,51,0,0,     // n
+	0,0,0,30,51,51,51,51,30,0,0,     // o
+	0,0,0,30,51,51,51,31,3,3,0,      // p
+	0,0,0,30,51,51,51,62,48,48,0,    // q
+	0,0,0,29,55,51,3,3,7,0,0,        // r
+	0,0,0,30,51,6,24,51,30,0,0,      // s
+	4,6,6,15,6,6,6,54,28,0,0,        // t
+	0,0,0,27,27,27,27,27,54,0,0,     // u
+	0,0,0,51,51,51,51,30,12,0,0,     // v
+	0,0,0,51,51,51,63,63,18,0,0,     // w
+	0,0,0,51,30,12,12,30,51,0,0,     // x
+	0,0,0,51,51,51,62,48,24,15,0,    // y
+	0,0,0,63,27,12,6,51,63,0,0,      // z
+
+	56,12,12,12,7,12,12,12,56,0,0,   // {
+	12,12,12,12,12,12,12,12,12,0,0,  // |
+	7,12,12,12,56,12,12,12,7,0,0,    // }
+	38,45,25,0,0,0,0,0,0,0,0,        // ~
+};
+
+void drawChar(uint8_t x, uint8_t y, color_t color, char c)
+{
+	cursor(x, y, 8, 11);
+	oledCommand(0x5c);
+	int i = c * 11;
+	for (int y = 0; y < 11; y++)
+	{
+		for (int x = 0; x < 8; x++)
+		{
+			if ( (map[i] & _BV(x)) != 0)
+			{
+				oledData(color.b);
+				oledData(color.g);
+				oledData(color.r);
+			}
+			else
+			{
+				oledData(0);
+				oledData(0);
+				oledData(0);
+			}
+		}
+		i++;
+	}
+}
+
+void drawString(uint8_t x, uint8_t y, color_t color, const char* str)
+{
+	for (uint8_t i = 0; i < strlen(str); i++)
+	{
+		drawChar(x + (i * 8), y, color, str[i]);
+	}
+}
+
+void screenTest()
+{
 	drawRect(0, 0, 128, 128, OLED_RED);
 	_delay_ms(500);
 	drawRect(0, 0, 128, 128, OLED_WHITE);
-	_delay_ms(500);
 	uint8_t k = 0;
 	uint8_t m = 0;
 	uint8_t l = 0;
@@ -210,45 +423,8 @@ void oledTest()
 			}
 		}
 	}
-
-
-	_delay_ms(1000);
-
-	oledCommand(0xb2);
-	oledData(0xa4);
-	oledData(0x00);
-	oledData(0x00);
-
-	_delay_ms(3000);
-
-	drawRect(0, 0, 128, 128, OLED_RED);
 	_delay_ms(500);
-	drawRect(0, 0, 128, 128, OLED_WHITE);
-	_delay_ms(500);
-	k = 0;
-	m = 0;
-	l = 0;
-	for (int i = 0; i < 12; i++)
-	{
-		for (int j = 0; j < 16; j++)
-		{
-			if ( i < 4 )
-			{
-				drawRect(j * 8, i * 10, 8, 10,  k++, 0, 0);
-			}
-			else if ( i >= 4 && i < 8)
-			{
-				drawRect(j * 8, i * 10, 8, 10,  0, m++, 0);
-			}
-			else
-			{
-				drawRect(j * 8, i * 10, 8, 10,  0, 0, l++);
-			}
-		}
-	}
 }
-
-
 
 
 #endif
--- oled/src/main.c
+++ oled/src/main.c
@@ -7,133 +7,136 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include <common.h>
-#include <uart.h>
-#include <spi.h>
 #include <oled.h>
+#include <dht11.h>
 
-//global variables
-extern char esc;
-extern char lbracket;
-
-extern char prompt[20];
-
-typedef struct {
-    char text[CMDSIZE];
-}Console;
-
-extern Console console[HISTSIZE];
-extern char cmdbuf[CMDSIZE];
-
-// cursors position
-extern int x, y;
-extern int cy;
-
-void clearScreen();
-void setCursor(int x, int y);
-void key(char c);
-
-#define CMD_NUM 3
-char *cmdList[CMD_NUM] =
+uint8_t DHT11_GetData(uint8_t *data1, uint8_t *data2)
 {
-    "clear",
-    "ls",
-    "spi",
-};
+	uint8_t bits1[5];
+	uint8_t bits2[5];
+	uint8_t i, j = 0;
 
-void func(char *args)
-{
-    char *token;
-    char *rest;
-    char str[CMDSIZE];
-    char *argv[10];
-    int argc = 0;
-    int i;
+	//reset port
+	DHT11_DDR |= _BV(DHT11_INPUTPIN1) | _BV(DHT11_INPUTPIN2); //output
+	DHT11_PORT |= _BV(DHT11_INPUTPIN1) | _BV(DHT11_INPUTPIN2); //high
+	_delay_ms(100);
 
-    memset(str, '\0', sizeof(str));
-    strcpy(str, args);
-    rest = str;
+	//send request
+	DHT11_PORT &= ~_BV(DHT11_INPUTPIN1); //low
+	_delay_ms(18);
+	DHT11_PORT |= _BV(DHT11_INPUTPIN1); //high
+	_delay_us(1);
+	DHT11_DDR &= ~_BV(DHT11_INPUTPIN1); //input
+	_delay_us(39);
 
-    // tokenizing
-    while((token = strtok_r(rest, " ", &rest)))
-    {
-        int len = strlen(token);
-        argv[argc] = (char*) malloc (len + 1);
-        strcpy(argv[argc], token);
-        argv[argc][len] = '\0';
-        argc++;
-    }
+	//check start condition 1
+	if ((DHT11_PIN & _BV(DHT11_INPUTPIN1))) {
+		return 1;
+	}
+	_delay_us(80);
+	//check start condition 2
+	if (!(DHT11_PIN & _BV(DHT11_INPUTPIN1))) {
+		return 2;
+	}
+	_delay_us(80);
 
-    char *cmd = argv[0];
-
-    // processing command
-    
-	
-	// clear screen
-    if (strcmp(cmd, "clear") == 0)
-    {
-        clearScreen();
-    }
-    // check command
-    else if (strcmp(cmd, "ls") == 0)
-    {
-        for (i = 0; i < CMD_NUM; i++)
-        {
-            printf("%10s", cmdList[i]);
-        }
-        putchar('\n');
-    }
-	else if (strcmp(cmd, "spi") == 0)
-	{
-		int len = 1;
-		unsigned char tx[len];
-		unsigned char rx[len];
-		if (argc == 2)
-		{
-			tx[0] = atoi(argv[3]);
-			spiTranceive(tx, rx, len);
+	//read the data
+	for (j = 0; j < 5; j++) { //read 5 byte
+		uint8_t result = 0;
+		for (i = 0; i < 8; i++) { //read every bit
+			while (!(DHT11_PIN & _BV(DHT11_INPUTPIN1)))
+				; //wait for an high input
+			_delay_us(30);
+			if (DHT11_PIN & _BV(DHT11_INPUTPIN1)) //if input is high after 30 us, get result
+				result |= (1 << (7 - i));
+			while (DHT11_PIN & _BV(DHT11_INPUTPIN1))
+				; //wait until input get low
 		}
-		else
-		{
-			printf("format err : spi [data]\n");
-		}
-		printf("spi ret = %02x\n", rx[len -1]);
+		bits1[j] = result;
 
 	}
-    // error handling
-    else
-    {
-        printf("undefined command \"%s\"\n", cmd);
-    }
 
-    // free memory
-    for (i = 0; i < argc; i++)
-    {
-        free(argv[i]);
-    }
+	//send request
+	DHT11_PORT &= ~_BV(DHT11_INPUTPIN2); //low
+	_delay_ms(18);
+	DHT11_PORT |= _BV(DHT11_INPUTPIN2); //high
+	_delay_us(1);
+	DHT11_DDR &= ~_BV(DHT11_INPUTPIN2); //input
+	_delay_us(39);
+
+	//check start condition 1
+	if ((DHT11_PIN & _BV(DHT11_INPUTPIN2))) {
+		return 1;
+	}
+	_delay_us(80);
+	//check start condition 2
+	if (!(DHT11_PIN & _BV(DHT11_INPUTPIN2))) {
+		return 2;
+	}
+	_delay_us(80);
+	for (j = 0; j < 5; j++) { //read 5 byte
+		uint8_t result = 0;
+		for (i = 0; i < 8; i++) { //read every bit
+			while (!(DHT11_PIN & _BV(DHT11_INPUTPIN2)))
+				; //wait for an high input
+			_delay_us(30);
+			if (DHT11_PIN & _BV(DHT11_INPUTPIN2)) //if input is high after 30 us, get result
+				result |= (1 << (7 - i));
+			while (DHT11_PIN & _BV(DHT11_INPUTPIN2))
+				; //wait until input get low
+		}
+		bits2[j] = result;
+	}
+
+	//reset port
+	DHT11_DDR |= _BV(DHT11_INPUTPIN1) | _BV(DHT11_INPUTPIN2); //output
+	DHT11_PORT |= _BV(DHT11_INPUTPIN1) | _BV(DHT11_INPUTPIN2); //low
+	_delay_ms(100);
+
+	//check checksum
+	if (bits1[0] + bits1[1] + bits1[2] + bits1[3] == bits1[4] &&
+			bits2[0] + bits2[1] + bits2[2] + bits2[3] == bits2[4])
+	{
+		data1[0] = bits1[2];
+		data1[1] = bits1[0];
+		data2[0] = bits2[2];
+		data2[1] = bits2[0];
+		return 0;
+	}
+
+	return 3;
 }
+
 
 int main(void)
 {
-    // UART init
-	// uart_init();
+	_delay_ms(1000);
 
 	oledInit();
 
-	oledTest();
+	uint8_t data1[2];
+	uint8_t data2[2];
 
-	while (1)
+	uint8_t err;
+	for (;;)
 	{
+		color_t color = {0, 127, 0};
+		if ((err = DHT11_GetData(data1, data2)) == 0)
+		{
+			char tmp[17];
+			sprintf(tmp, "t : %03d  h : %03d", data1[0], data1[1]);
+			drawString(0, 0, color, tmp);
+			sprintf(tmp, "t : %03d  h : %03d", data2[0], data2[1]);
+			drawString(0, 11, color, tmp);
+		}
+		else
+		{
+			drawRect(0, 0, 128, 22, OLED_BLACK);
+			drawString(0, 11, color, "err");
+		}
+		_delay_ms(1000);
 	}
-	clearScreen();
 
-	printf("%s\n", prompt);
-
-	// main loop
-	for(;;){
-		char c = getchar();
-		key(c);
-	}
     return 0;
 }
 
